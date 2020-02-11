5d6d51e4b7ab49fb9e99c91d69801024148ce34f
cas432 <cas432@georgetown.edu>
2020-02-10 20:37:42 -0500

    completed per pound pricing

--- README.md
+++ README.md
@@ -1,19 +1,34 @@
 # Shopping Cart Project
 
-[Project Description]
-https://github.com/prof-rossetti/intro-to-python/tree/master/projects/shopping-cart
+[Project Description](https://github.com/prof-rossetti/intro-to-python/tree/master/projects/shopping-cart)
 
 ## Installation
 
-Clone or download from [GitHub Source] (https://github.com/cas432/shopping-cart), then navigate into the project repository:
+Fork this [repository](https://github.com/cas432/shopping-cart) from Github and then clone your fork to download it locally onto your computer. Then navigate there from the command line:
 
-'''sh
-cd shopping-cart
-'''
+```sh
+cd ~/Desktop/shopping-cart
+```
+
+## Setup
+
+### Environment Setup
+Setup the virtual environment "shopping-env":
+
+```sh
+conda create -n shopping-env python=3.7 #(first time only)
+conda activate shopping-env
+```
+### Package Installation
+Please install the following packages
+
+```sh
+pip install python-dotenv
+```
 
 ## Usage
 Run the program:
 
-'''py
+```py
 python shopping_cart.py
-'''
+```
--- shopping_cart.py
+++ shopping_cart.py
@@ -4,35 +4,40 @@ from dotenv import load_dotenv
 import os
 
 products = [
-    {"id":1, "name": "Chocolate Sandwich Cookies", "department": "snacks", "aisle": "cookies cakes", "price": 3.50},
-    {"id":2, "name": "All-Seasons Salt", "department": "pantry", "aisle": "spices seasonings", "price": 4.99},
-    {"id":3, "name": "Robust Golden Unsweetened Oolong Tea", "department": "beverages", "aisle": "tea", "price": 2.49},
-    {"id":4, "name": "Smart Ones Classic Favorites Mini Rigatoni With Vodka Cream Sauce", "department": "frozen", "aisle": "frozen meals", "price": 6.99},
-    {"id":5, "name": "Green Chile Anytime Sauce", "department": "pantry", "aisle": "marinades meat preparation", "price": 7.99},
-    {"id":6, "name": "Dry Nose Oil", "department": "personal care", "aisle": "cold flu allergy", "price": 21.99},
-    {"id":7, "name": "Pure Coconut Water With Orange", "department": "beverages", "aisle": "juice nectars", "price": 3.50},
-    {"id":8, "name": "Cut Russet Potatoes Steam N' Mash", "department": "frozen", "aisle": "frozen produce", "price": 4.25},
-    {"id":9, "name": "Light Strawberry Blueberry Yogurt", "department": "dairy eggs", "aisle": "yogurt", "price": 6.50},
-    {"id":10, "name": "Sparkling Orange Juice & Prickly Pear Beverage", "department": "beverages", "aisle": "water seltzer sparkling water", "price": 2.99},
-    {"id":11, "name": "Peach Mango Juice", "department": "beverages", "aisle": "refrigerated", "price": 1.99},
-    {"id":12, "name": "Chocolate Fudge Layer Cake", "department": "frozen", "aisle": "frozen dessert", "price": 18.50},
-    {"id":13, "name": "Saline Nasal Mist", "department": "personal care", "aisle": "cold flu allergy", "price": 16.00},
-    {"id":14, "name": "Fresh Scent Dishwasher Cleaner", "department": "household", "aisle": "dish detergents", "price": 4.99},
-    {"id":15, "name": "Overnight Diapers Size 6", "department": "babies", "aisle": "diapers wipes", "price": 25.50},
-    {"id":16, "name": "Mint Chocolate Flavored Syrup", "department": "snacks", "aisle": "ice cream toppings", "price": 4.50},
-    {"id":17, "name": "Rendered Duck Fat", "department": "meat seafood", "aisle": "poultry counter", "price": 9.99},
-    {"id":18, "name": "Pizza for One Suprema Frozen Pizza", "department": "frozen", "aisle": "frozen pizza", "price": 12.50},
-    {"id":19, "name": "Gluten Free Quinoa Three Cheese & Mushroom Blend", "department": "dry goods pasta", "aisle": "grains rice dried goods", "price": 3.99},
-    {"id":20, "name": "Pomegranate Cranberry & Aloe Vera Enrich Drink", "department": "beverages", "aisle": "juice nectars", "price": 4.25}
+    {"id":1, "name": "Chocolate Sandwich Cookies", "department": "snacks", "aisle": "cookies cakes", "price": 3.50, "price_per": "N"},
+    {"id":2, "name": "All-Seasons Salt", "department": "pantry", "aisle": "spices seasonings", "price": 4.99, "price_per": "N"},
+    {"id":3, "name": "Robust Golden Unsweetened Oolong Tea", "department": "beverages", "aisle": "tea", "price": 2.49, "price_per": "N"},
+    {"id":4, "name": "Smart Ones Classic Favorites Mini Rigatoni With Vodka Cream Sauce", "department": "frozen", "aisle": "frozen meals", "price": 6.99, "price_per": "N"},
+    {"id":5, "name": "Green Chile Anytime Sauce", "department": "pantry", "aisle": "marinades meat preparation", "price": 7.99, "price_per": "N"},
+    {"id":6, "name": "Dry Nose Oil", "department": "personal care", "aisle": "cold flu allergy", "price": 21.99, "price_per": "N"},
+    {"id":7, "name": "Pure Coconut Water With Orange", "department": "beverages", "aisle": "juice nectars", "price": 3.50, "price_per": "N"},
+    {"id":8, "name": "Cut Russet Potatoes Steam N' Mash", "department": "frozen", "aisle": "frozen produce", "price": 4.25, "price_per": "N"},
+    {"id":9, "name": "Light Strawberry Blueberry Yogurt", "department": "dairy eggs", "aisle": "yogurt", "price": 6.50, "price_per": "N"},
+    {"id":10, "name": "Sparkling Orange Juice & Prickly Pear Beverage", "department": "beverages", "aisle": "water seltzer sparkling water", "price": 2.99, "price_per": "N"},
+    {"id":11, "name": "Peach Mango Juice", "department": "beverages", "aisle": "refrigerated", "price": 1.99, "price_per": "N"},
+    {"id":12, "name": "Chocolate Fudge Layer Cake", "department": "frozen", "aisle": "frozen dessert", "price": 18.50, "price_per": "N"},
+    {"id":13, "name": "Saline Nasal Mist", "department": "personal care", "aisle": "cold flu allergy", "price": 16.00, "price_per": "N"},
+    {"id":14, "name": "Fresh Scent Dishwasher Cleaner", "department": "household", "aisle": "dish detergents", "price": 4.99, "price_per": "N"},
+    {"id":15, "name": "Overnight Diapers Size 6", "department": "babies", "aisle": "diapers wipes", "price": 25.50, "price_per": "N"},
+    {"id":16, "name": "Mint Chocolate Flavored Syrup", "department": "snacks", "aisle": "ice cream toppings", "price": 4.50, "price_per": "N"},
+    {"id":17, "name": "Rendered Duck Fat", "department": "meat seafood", "aisle": "poultry counter", "price": 9.99, "price_per": "N"},
+    {"id":18, "name": "Pizza for One Suprema Frozen Pizza", "department": "frozen", "aisle": "frozen pizza", "price": 12.50, "price_per": "N"},
+    {"id":19, "name": "Gluten Free Quinoa Three Cheese & Mushroom Blend", "department": "dry goods pasta", "aisle": "grains rice dried goods", "price": 3.99, "price_per": "N"},
+    {"id":20, "name": "Pomegranate Cranberry & Aloe Vera Enrich Drink", "department": "beverages", "aisle": "juice nectars", "price": 4.25, "price_per": "N"},
+    {"id":21, "name": "Organic Bananas", "department": "fruit", "aisle": "produce", "price": 0.79, "price_per": "Y"},
+    {"id":22, "name": "Organic Apples", "department": "fruit", "aisle": "produce", "price": 1.20, "price_per": "Y"}
 ] # based on data from Instacart: https://www.instacart.com/datasets/grocery-shopping-2017
 
 
+#BANANAS price per attribute : bool T/F Check for t/f then ask for #
 #PART I: CAPTURE USER INPUT
 
 #Setup Variables
 total_price = 0
+x = 0
 selected_ids = []
 all_ids = []
+lbs = []
 divider = "-------------------------"
 
 
@@ -46,10 +51,19 @@ for p in products:
 #Enter and Validate Inputs   
 while True: 
     selected_id = input("Please input a product identifier: ")
+                
    
     if selected_id in all_ids:
         selected_ids.append(selected_id)
-
+        int_selected_id = int(selected_id) 
+        int_selected_id_minus_1 = int_selected_id - 1
+                
+        if products[int_selected_id_minus_1].get("price_per") == "Y":
+            pounds = input("    Please input how many pounds you would like: ")
+            float_pounds = float(pounds)
+            lbs.append(float_pounds)
+        
+            
     elif selected_id == "DONE":
         break
 
@@ -69,7 +83,8 @@ hour = (time.strftime("%I:%M %p"))
 
 #Create Receipt File
 today_file_name = datetime.datetime.now().strftime("%Y-%m-%d-%H-%M-%S-%f")
-file_path = today_file_name + ".txt"
+file_path = "receipts/" + today_file_name + ".txt"
+
 
 
 #Print out Receipt/Write File
@@ -96,15 +111,37 @@ with open(file_path, "w") as file:
     #Print Individual Items and Prices
     print("SELECTED PRODUCT: ")
     file.write("\nSELECTED PRODUCT:")
-    for selected_id in selected_ids:
-        matching_products = [p for p in products if str(p["id"]) == str(selected_id)]
-        matching_product = matching_products[0]
-        total_price = total_price + matching_product["price"]
-
-        price_usd = "(${0:.2f})".format(matching_product["price"])
     
-        print("... " + matching_product["name"] + " " + price_usd)
-        file.write("\n... " + matching_product["name"] + " " + price_usd)
+    
+    
+    for selected_id in selected_ids:
+            
+        matching_products = [p for p in products if str(p["id"]) == str(selected_id)]
+         
+        matching_product = matching_products[0]
+
+        if matching_product["price_per"] == "N":
+            total_price = total_price + matching_product["price"]
+        
+         
+            price_usd = "(${0:.2f})".format(matching_product["price"])
+                
+            print("... " + matching_product["name"] + " " + price_usd)
+            file.write("\n... " + matching_product["name"] + " " + price_usd)
+
+         
+        elif matching_product["price_per"] == "Y":
+           
+            new_pound_price = matching_product["price"] * lbs[x]
+            total_price = total_price + new_pound_price
+
+            price_usd = "(${0:.2f})".format(new_pound_price)
+    
+            print("... " + matching_product["name"] + " " + price_usd)
+            file.write("\n... " + matching_product["name"] + " " + price_usd)
+            x = x + 1
+
+
     
     file.write("\n")
     file.write(divider)
@@ -131,11 +168,6 @@ with open(file_path, "w") as file:
     file.write("\nTOTAL: " + tax_plus_total_usd)
 
     
-    print(divider)
-    email_YN = input("Would you like to have your receipt emailed to you? (Y/N): ")
-        if email_YN == "Y":
-            user_email = input("Please enter your email address: ")
-                   
 
 
     print(divider)
@@ -146,3 +178,5 @@ with open(file_path, "w") as file:
     file.write(divider)
     file.write("\nTHANK YOU! SEE YOU AGAIN SOON!\n")
     file.write(divider)
+
+#os.rename(today_file_name + ".txt", "receipts/" + today_file_name + ".txt")
